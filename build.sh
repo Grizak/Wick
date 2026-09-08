385e7140b76e8c81c80b988e97f938adde64b0bb
Isak Grönlund <gronlundisak11@gmail.com>
2026-09-08 13:00:56 +0200

    Changed build.sh to add some debug output

--- build.sh
+++ build.sh
@@ -4,8 +4,18 @@ RELEASE=$WICK_RELEASE
 TARGET=$1
 SOURCES=${@:2}
 
+printf "%s " "Building a" 
+if [[ -z $RELEASE ]]; then
+ printf "development"
+else
+ printf "release"
+fi
+echo " build of WICK for $GOOS/$GOARCH"
+
 if [ -z $RELEASE ]; then # If !RELEASE
   go build -ldflags "-X main.version=$(git describe --tags --abbrev=0)" -o $TARGET $SOURCES
+  echo "Done"
 else
   go build -ldflags "-X main.version=$(git describe --tags --abbrev=0) -s -w" -o $TARGET $SOURCES # Build without debug symbols (-s -w)
+  echo "Done"
 fi
